830bd7a3e6a989258609d69c1c06812e03fae478
wefwwefw <goldifyed@gmail.com>
2025-06-04 16:22:59 +0200

    questions

---
+++ schnupperlehre/app/test.js
@@ -0,0 +1,43 @@
+
+
+let a = 5;
+let b = 10;
+let summe = a + b;
+console.log("Summe:",summe);
+
+console.log (1+1);// Aufgabe: 2
+
+function greet(name) {
+console.log("Hallo, " + name + "!");
+}
+greet("john");
+
+
+class Person {constructor (first, last, age, eye) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eye;
+}}
+const user = new Person("john","peter","25", "blue");
+console.log("Name: " + user.name);
+console.log("Alter: " + user.age);
+
+const readline = require("readline-sync");
+
+let person= readline.question("Wie alt bist du");
+console.log("super schon " + person);
+
+ let username= readline.question("Wie heisst du? ");
+  console.log("Hallo " + username);
+  if (user === "18") {
+    console.log("du bist sehr alt") }
+let hobbbby= readline.question("Was ist dein hobby? ");
+  console.log("Ich liebe " + hobbbby);
+  if (user === "Basketball!") {
+    console.log("Ich liebe Basketball!") }   
+
+    let jung= readline.question("Was ist deine Augenfarbe?");
+  console.log("Okay, sie sind " + jung);
+  
+  
